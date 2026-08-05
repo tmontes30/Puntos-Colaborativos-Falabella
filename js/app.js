4d966148d0c46c7647cb95b6dc0faa0098738847
tmontes30 <tmontes30@users.noreply.github.com>
2026-08-05 10:55:11 -0400

    Volver a tiles claros de OpenStreetMap, manteniendo la interfaz oscura
    El mapa oscuro dificultaba distinguir calles/referencias. Ahora solo la
    UI (topbar, sidebar, popups, controles) queda en tema oscuro; el mapa
    en si vuelve a los tiles estandar de OSM para mayor legibilidad.

--- js/app.js
+++ js/app.js
@@ -13,11 +13,9 @@
   };
 
   const map = L.map("map").setView(SANTIAGO_CENTER, 11);
-  L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
-    maxZoom: 20,
-    subdomains: "abcd",
-    attribution:
-      "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors &copy; <a href='https://carto.com/attributions'>CARTO</a>",
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors",
   }).addTo(map);
 
   let bodegaMarker = null;
